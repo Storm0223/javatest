1832d2deb999c172f161cce54521bda40d496091
Storm0223 <storm0223@foxmail.com>
2020-01-22 13:10:22 +0800

    hello

--- src/main/java/Hi.java
+++ src/main/java/Hi.java
@@ -3,6 +3,9 @@ import java.util.List;
 import java.util.Stack;
 
 public class Hi {
-    Stack stack = new Stack();
-    List list = new ArrayList<String>();
+    public static void main(String[] args) {
+        Stack stack = new Stack();
+        List list = new ArrayList<String>();
+    }
+
 }
